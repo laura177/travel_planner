6a5610b0255f96e46c1950f255b16170e63a876d
Laura177 <laura.c.buffington@gmail.com>
2022-08-22 15:56:51 -0700

    initial commit - install auth pkg

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.onboarding_travel_planner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,7 @@
+/// {@template authentication_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class AuthenticationRepository {
+  /// {@macro authentication_repository}
+  const AuthenticationRepository();
+}
