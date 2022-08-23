2d71c76a70c7bad612b797d40230f13cf8f52514
Laura177 <laura.c.buffington@gmail.com>
2022-08-23 13:06:42 -0700

    set up flutter-fire, connect to firebase

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -4,12 +4,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:onboarding_travel_planner/app/app.dart';
 import 'package:onboarding_travel_planner/bootstrap.dart';
+import 'package:onboarding_travel_planner/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Bloc.observer = AppBlocObserver();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final authenticationRepository = AuthenticationRepository();
   await authenticationRepository.user.first;
